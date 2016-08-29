84eb1c54bc327385c8264a33583b9d729fff4662
simon <github@simongrondin.name>
2016-08-29 18:04:53 -0400

    0.0.1 First version

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # umzug-sync
-Sequelize (PG) migrations for distributed systems
+
+Sequelize (Postgresql only) migrations for distributed systems. This library uses `umzug`, the library used by Sequelize to run migrations. Usually those migrations are run from the command line tool, `sequelize-cli`. This library makes it easy and safe to run migrations from the application code across a cluster of servers. All servers can call `usync.migrate()` and wait on the promise and be guaranteed upon resolution that the database schema is fully up to date.
+
+Umzug-sync is resilient against race conditions, conflicts, concurrent migrations, timeouts and many other issues that may occur in distributed environments.
+
+```js
+var usync = require('umzug-sync')
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize({ /* sequelize options */ })
+
+var config = {
+  sequelize: sequelize,
+  SequelizeImport: Sequelize,
+  migrationsDir: 'migrations/'
+}
+
+usync.migrate(config)
+.then(function () {
+  // Execute program
+})
+.catch(function (err) {
+  // Handle the error, something bad happened
+})
+```
+
+There is only one function: `migrate()`. It takes a `config` object and returns a promise.
+
+### config
+
+The config object supports the following options:
+
+| Name | Type | Mandatory | Default | Description |
+|------|------|-----------|---------|-------------|
+| `sequelize` | Object | Yes | | A configured instance of `Sequelize`. |
+| `SequelizeImport` | Object | Yes | | The Sequelize library object that created the `sequelize` instance. |
+| `migrationsDir` | String | Yes | | Path (relative or absolute) where the migration files are located. Those files must end in `.js`. |
+| `chdir` | String | No | | Sometimes migration files load other files relatively to a certain assumed Current Working Directory (cwd/pwd). Setting this option to a path will change the cwd/pwd of the process for the duration of the `migrate()` function execution and change it back afterwards. |
+| `logging` | Function | No | | Function that will be called with a single `string` argument whenever a migration event occurs. Example value: `console.log` |
+| `timeout` | Integer | No | 15 | Maximum time (in seconds) to wait for the mutex. `migrate()` is guaranteed to resolve/reject within `2 * timeout` |
+
+### **WARNING**
+
+Race conditions are possible if the `timeout` expires **before** the migrations are done executing. This is **by design** in case a migration gets "jammed" or the server simply disappears mid-migration(s). This way another server will get a chance to run the migrations instead of leaving the whole system in a broken state. So pick the `timeout` value carefully.
---
+++ index.js
@@ -0,0 +1,126 @@
+'use strict'
+
+var Umzug = require('umzug')
+var Promise = require('bluebird')
+var util = require('util')
+
+function ExitEarly () { this.custom = true }
+ExitEarly.prototype = Object.create(Error.prototype)
+function ExitTimeout () { this.custom = true }
+ExitTimeout.prototype = Object.create(Error.prototype)
+function ExitError (message) { this.message = message; this.custom = true }
+ExitError.prototype = Object.create(Error.prototype)
+
+var mutexTableName = 'SequelizeMetaMutexes' // Intentionally hardcoded
+var createMutexTable = function (sequelize) {
+  var sql = util.format('CREATE TABLE IF NOT EXISTS "%s" ("mutex" INTEGER NOT NULL UNIQUE, "ts" TIMESTAMP WITHOUT TIME ZONE NOT NULL, "id" TEXT NOT NULL, PRIMARY KEY ("mutex"))', mutexTableName)
+  return sequelize.query(sql)
+}
+var acquireLock = function (sequelize, SequelizeImport, lockTimeout) {
+  return sequelize.transaction({
+    isolationLevel: SequelizeImport.Transaction.ISOLATION_LEVELS.SERIALIZABLE
+  }, function (t) {
+    return sequelize.query(util.format('LOCK TABLE "%s" IN ACCESS EXCLUSIVE MODE NOWAIT', mutexTableName), { transaction: t })
+    .then(function () {
+      return sequelize.query(util.format('DELETE FROM "%s" WHERE "ts" < (NOW() - INTERVAL \'%d SECONDS\')', mutexTableName, lockTimeout), { transaction: t })
+    })
+    .then(function () {
+      // Fully hardcoded, creates a collision on purpose
+      return sequelize.query(util.format('INSERT INTO "%s" ("mutex", "ts", "id") VALUES (1, NOW(), MD5(NOW()::TEXT)) RETURNING *', mutexTableName), { transaction: t })
+    })
+    .spread(function (row) {
+      return Promise.resolve(row.id)
+    })
+  })
+}
+var block = function (until, umzug, sequelize, SequelizeImport, lockTimeout) {
+  return umzug.pending()
+  .catch(function (err) {
+    throw new ExitError(err.message)
+  })
+  .then(function (migrations) {
+    if (migrations.length === 0) {
+      throw new ExitEarly()
+    }
+    if (Date.now() >= until) {
+      throw new ExitTimeout()
+    }
+    return acquireLock(sequelize, SequelizeImport, lockTimeout)
+  })
+  .catch(function (err) {
+    if (err.custom) {
+      return Promise.reject(err)
+    }
+
+    return Promise.delay(1500)
+    .then(function () {
+      return block(until, umzug, sequelize, SequelizeImport, lockTimeout)
+    })
+  })
+}
+var releaseLock = function (sequelize, id) {
+  return sequelize.query(util.format('DELETE FROM "%s" WHERE "id" = \'%s\'', mutexTableName, id))
+}
+
+exports.migrate = function (params) {
+  var lockTimeout = (Number.isInteger(params.timeout) && params.timeout > 0) ? params.timeout : 15
+  var systemTimeout = lockTimeout * 2
+  var config = {
+    storage: 'sequelize',
+    storageOptions: {
+      sequelize: params.sequelize
+    },
+    logging: params.logging == null ? false : params.logging,
+    migrations: {
+      params: [ params.sequelize.getQueryInterface(), params.SequelizeImport ],
+      path: params.migrationsDir,
+      pattern: /.+\.js$/ // or else migration(s) with period(s) in their name doesn't work
+    }
+  }
+  var cwd = process.cwd()
+  if (params.chdir) {
+    process.chdir(params.chdir)
+  }
+  var umzug = new Umzug(config)
+
+  var mutex = ''
+  return params.sequelize.authenticate()
+  .then(function () {
+    return createMutexTable(params.sequelize)
+  })
+  .then(function () {
+    var until = Date.now() + (systemTimeout * 1000)
+    return block(until, umzug, params.sequelize, params.SequelizeImport, lockTimeout)
+  })
+  .then(function (id) {
+    mutex = id
+    return umzug.up()
+  })
+  .then(function () {
+    // Always reset the chdir
+    if (params.chdir) {
+      process.chdir(cwd)
+    }
+    return releaseLock(params.sequelize, mutex)
+  })
+  .catch(ExitEarly, function () {
+    return Promise.resolve()
+  })
+  .then(function () {
+    // Always reset the chdir
+    if (params.chdir) {
+      process.chdir(cwd)
+    }
+    return Promise.resolve()
+  })
+  .catch(ExitTimeout, function () {
+    return Promise.reject(new Error('Could not execute the migrations within ' + systemTimeout + ' seconds (2 * timeout).'))
+  })
+  .catch(function (err) {
+    // Always reset the chdir
+    if (params.chdir) {
+      process.chdir(cwd)
+    }
+    return Promise.reject(err)
+  })
+}
